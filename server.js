e70734d8d6d0d1378130a39005d0bc6a3d1aa154
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-05-01 13:02:30 +0200

    api for golden globe and flowers

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+
+//import the data
+import flowers from "./data/flowers.json";
+import data from "./data/golden-globes.json";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
@@ -15,6 +20,48 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+//get all flowers
+//http://localhost:8090/flowers
+app.get("/flowers", (req, res) => {
+  res.json(flowers);
+
+}
+);
+
+
+app.get("/nominations" , (req, res) => {
+  res.json(data);
+
+});
+
+// get films by year and winner
+//http://localhost:8090/year/2020?won=true
+app.get("/year/:year", (req, res) => {
+  const year = req.params.year;
+  const showWon = req.query.won;
+  let nominationsFromYear = data.filter((item) => item.year_award === +year);
+
+if (showWon) {
+  nominationsFromYear = nominationsFromYear.filter((item) => item.win);
+}
+
+  res.json(nominationsFromYear);
+});
+
+
+// get one flower by id
+app.get("/flowers/:flowerId", (req, res) => {
+  const flowerId = req.params.flowerId;
+  const flower = flowers.find((flower) => flower.id === +flowerId);
+  if (flower) {
+    res.json(flower);
+  } else {
+    res.status(404).send({ message: "Not found" });
+  }
+}
+
+);
 
 // Start the server
 app.listen(port, () => {
